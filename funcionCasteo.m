d0cd17bf6c0bcd6ebfccd31ac8b66de674c7d837
Mati <matimorsa@hotmail.com>
2017-10-14 02:52:32 -0300

    Creo funcion de casteo que recibe los datos del usuario y los devuelve en una matriz de la manera [x,y]

---
+++ funcionCasteo.m
@@ -0,0 +1,30 @@
+
+function varRet = funcionCasteo (datos);
+
+  coordenadas_x = [];
+  coordenadas_y = [];
+
+  contador = 0;
+
+  variableArray = "((1,-2),(3,2))";
+
+  for i= 1:length(variableArray)
+    if((variableArray(i) == "0")||(variableArray(i)== "1")||(variableArray(i)== "2")||(variableArray(i)== "3")||(variableArray(i)== "4")||(variableArray(i)== "5")||(variableArray(i)== "6")||(variableArray(i)== "7")||(variableArray(i)== "8")||(variableArray(i)== "9"))
+      variable = str2num(variableArray(i))
+      if (variableArray(i-1) == "-");
+        variable = variable *(-1);  
+      endif
+      switch contador
+        case 0
+        coordenadas_x = [coordenadas_x variable];
+        contador = 1;
+        case 1
+        coordenadas_y = [coordenadas_y variable];
+        contador = 0;
+      endswitch
+    endif
+  endfor
+  
+  #Devuelve una matriz de la manera [X ,Y]
+  varRet = [coordenadas_x'  coordenadas_y'];
+  return
